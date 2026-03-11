a84185c4e69f6b39e541d5e57d6dc7823548b104
Erik Kasten <e.kasten@ostfalia.de>
2026-03-11 17:38:39 +0000

    feat: implement login page layout and form with Angular Material components

--- awd-frontend/src/app/pages/login/login.ts
+++ awd-frontend/src/app/pages/login/login.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './login.html'
 })
 export class Login {}
