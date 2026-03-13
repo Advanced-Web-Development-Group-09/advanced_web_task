e7bbb4d93d37b5618cbca8dc88c58624f4247397
Erik Kasten <e.kasten@ostfalia.de>
2026-03-13 16:32:34 +0000

    feat: enhance profile page with user details and card layout

--- awd-frontend/src/app/pages/profile/profile.ts
+++ awd-frontend/src/app/pages/profile/profile.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {Sidenav} from '../../shared/sidenav/sidenav';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-profile',
-  imports: [MatSidenavModule, Sidenav],
+  imports: [MatSidenavModule, Sidenav, MatCardModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
