cce16b3a86f2b7bb1f535de8759d9a5ea6859e8a
Erik Kasten <e.kasten@ostfalia.de>
2026-03-14 10:02:10 +0000

    feat: enhance analytics page with form controls and card layout for data input

--- awd-frontend/src/app/pages/analytics/analytics.ts
+++ awd-frontend/src/app/pages/analytics/analytics.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {Sidenav} from '../../shared/sidenav/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Sidenav } from '../../shared/sidenav/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-analytics',
-  imports: [MatSidenavModule, Sidenav],
+  imports: [
+    MatSidenavModule,
+    Sidenav,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './analytics.html',
   styleUrl: './analytics.css',
 })
-export class Analytics {}
+export class Analytics {
+  myControl = new FormControl('');
+  options: string[] = ['Hildesheim Hbf', 'Hannover Hbf', 'Braunschweig Hbf', 'Göttingen Hbf'];
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) => option.toLowerCase().includes(filterValue));
+  }
+}
