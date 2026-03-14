38d965662f62d3efd9ff5684d2b51e8f08c3a1e0
Erik Kasten <e.kasten@ostfalia.de>
2026-03-14 09:45:52 +0000

    style: format CSS and HTML for consistency; improve TypeScript code readability

--- awd-frontend/src/app/pages/datasets/datasets.ts
+++ awd-frontend/src/app/pages/datasets/datasets.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {Sidenav} from '../../shared/sidenav/sidenav';
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Sidenav } from '../../shared/sidenav/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 export interface DatasetElement {
   name: string;
@@ -13,24 +13,31 @@ export interface DatasetElement {
 }
 
 const UPLOADED_DATA: DatasetElement[] = [
-  {name: 'data_set_1.csv', size: '145mb', date: '2023-01-01 00:01:00'},
-  {name: 'data_set_2.csv', size: '90mb', date: '2023-01-02 00:17:00'},
-  {name: 'data_set_3.csv', size: '137mb', date: '2023-01-03 00:04:00'},
-  {name: 'data_set_4.csv', size: '49mb', date: '2023-01-04 00:21:00'},
-  {name: 'data_set_5.csv', size: '3mb', date: '2023-01-05 00:21:00'},
+  { name: 'data_set_1.csv', size: '145mb', date: '2023-01-01 00:01:00' },
+  { name: 'data_set_2.csv', size: '90mb', date: '2023-01-02 00:17:00' },
+  { name: 'data_set_3.csv', size: '137mb', date: '2023-01-03 00:04:00' },
+  { name: 'data_set_4.csv', size: '49mb', date: '2023-01-04 00:21:00' },
+  { name: 'data_set_5.csv', size: '3mb', date: '2023-01-05 00:21:00' },
 ];
 
 const EXPORTED_DATA: DatasetElement[] = [
-  {name: 'export_1.csv', size: '3mb', date: '2023-01-01 00:01:00'},
-  {name: 'export_2.csv', size: '1mb', date: '2023-01-02 00:17:00'},
-  {name: 'export_3.csv', size: '7mb', date: '2023-01-03 00:04:00'},
-  {name: 'export_4.csv', size: '0.5mb', date: '2023-01-04 00:21:00'},
-  {name: 'export_5.csv', size: '10mb', date: '2023-01-05 00:21:00'},
+  { name: 'export_1.csv', size: '3mb', date: '2023-01-01 00:01:00' },
+  { name: 'export_2.csv', size: '1mb', date: '2023-01-02 00:17:00' },
+  { name: 'export_3.csv', size: '7mb', date: '2023-01-03 00:04:00' },
+  { name: 'export_4.csv', size: '0.5mb', date: '2023-01-04 00:21:00' },
+  { name: 'export_5.csv', size: '10mb', date: '2023-01-05 00:21:00' },
 ];
 
 @Component({
   selector: 'app-datasets',
-  imports: [MatSidenavModule, Sidenav, MatCardModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatSidenavModule,
+    Sidenav,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './datasets.html',
   styleUrl: './datasets.css',
 })
@@ -52,12 +59,16 @@ export class Datasets {
   }
 
   onDrop(event: DragEvent) {
-  event.preventDefault();
-  this.isDragging = false;
+    event.preventDefault();
+    this.isDragging = false;
 
-  if (event.dataTransfer?.files.length) {
+    if (event.dataTransfer?.files.length) {
       const file = event.dataTransfer.files[0];
-      if (!file.name.endsWith('.csv') && !file.name.endsWith('.xlsx') && !file.name.endsWith('.json')) {
+      if (
+        !file.name.endsWith('.csv') &&
+        !file.name.endsWith('.xlsx') &&
+        !file.name.endsWith('.json')
+      ) {
         alert('Only CSV, XLSX, and JSON files allowed');
         return;
       }
@@ -70,7 +81,11 @@ export class Datasets {
 
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
-      if (!file.name.endsWith('.csv') && !file.name.endsWith('.xlsx') && !file.name.endsWith('.json')) {
+      if (
+        !file.name.endsWith('.csv') &&
+        !file.name.endsWith('.xlsx') &&
+        !file.name.endsWith('.json')
+      ) {
         alert('Only CSV, XLSX, and JSON files allowed');
         return;
       }
