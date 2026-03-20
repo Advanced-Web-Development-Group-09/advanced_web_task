b6f1791c96e9509118a2075ff3cbbd869a1ad10a
Erik Kasten <e.kasten@ostfalia.de>
2026-03-20 14:31:37 +0000

    feat: update sidenav UI with button styles and layout adjustments

--- awd-frontend/src/app/shared/sidenav/sidenav.ts
+++ awd-frontend/src/app/shared/sidenav/sidenav.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sidenav',
   imports: [MatIconModule, MatButtonModule],
-  templateUrl: './sidenav.html'
+  templateUrl: './sidenav.html',
+  styleUrl: './sidenav.css',
 })
 export class Sidenav {}
