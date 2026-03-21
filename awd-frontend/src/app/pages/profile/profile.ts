3216d30d8a21891bb5d36c8f87072e1b316d9762
Erik Kasten <e.kasten@ostfalia.de>
2026-03-21 12:03:36 +0000

    feat: add internationalization support for status points in profile page

--- awd-frontend/src/app/pages/profile/profile.ts
+++ awd-frontend/src/app/pages/profile/profile.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {Sidenav} from '../../shared/sidenav/sidenav';
 import {MatCardModule} from '@angular/material/card';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [MatSidenavModule, Sidenav, MatCardModule],
+  imports: [MatSidenavModule, Sidenav, MatCardModule, TranslatePipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
