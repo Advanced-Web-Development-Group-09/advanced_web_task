5c39ab7216c7fd3a0f6ebd29bf7c3038a658a5f1
Erik Kasten <e.kasten@ostfalia.de>
2026-03-21 11:52:23 +0000

    feat: enhance login page with internationalization support for labels and messages

--- awd-frontend/src/app/pages/login/login.ts
+++ awd-frontend/src/app/pages/login/login.ts
@@ -4,10 +4,18 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, RouterLink],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    RouterLink,
+    TranslatePipe,
+  ],
   templateUrl: './login.html',
 })
 export class Login {}
