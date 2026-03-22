b222e00c6193371f67820aeacf70e3c96a496511
Erik Kasten <e.kasten@ostfalia.de>
2026-03-22 07:12:50 +0000

    feat: implement logout functionality in sidenav component

--- awd-frontend/src/app/shared/sidenav/sidenav.ts
+++ awd-frontend/src/app/shared/sidenav/sidenav.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
@@ -13,7 +13,10 @@ import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 export class Sidenav {
   isDarkMode = document.documentElement.classList.contains('dark-theme') ? 'dark' : 'light';
 
-  constructor(private translate: TranslateService) {}
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+  ) {}
 
   toggleLanguage() {
     const currentLang = this.translate.getCurrentLang() || this.translate.getFallbackLang();
@@ -25,5 +28,10 @@ export class Sidenav {
     this.isDarkMode = this.isDarkMode === 'light' ? 'dark' : 'light';
 
     document.documentElement.classList.toggle('dark-theme', this.isDarkMode === 'dark');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 }
